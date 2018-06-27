9fa0c68766f86afafadae4d847cd562cd424b074
马雅狄 <mayadi@travelsky.com>
2018-06-27 17:03:15 +0800

    work well in linux with zmq already installed

--- compilezmqfile.sh
+++ compilezmqfile.sh
@@ -1,1 +1,2 @@
-g++ -g -Wall -fPIC -I/home/mayadi/zmq/include -L/home/mayadi/zmq/lib -lzmq $1 -o $2
+#g++ -g -Wall -fPIC -I/home/mayadi/zmq/include -L/home/mayadi/zmq/lib -lzmq $1 -o $2
+g++ -g -Wall -fPIC -I/usr/include -L/usr/lib64 -lzmq $1 -o $2
