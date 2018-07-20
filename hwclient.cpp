f2da67ff2af1aa6354769d56e5a522cac0f34544
马雅狄 <mayadi@travelsky.com>
2018-07-20 17:16:55 +0800

    pipeline, multi-thread and sync

--- hwclient.cpp
+++ hwclient.cpp
@@ -7,8 +7,14 @@
 #include <string>
 #include <iostream>
 
-int main ()
+int main (int argc, char *argv[])
 {
+    bool bMulThread = false;
+    int nThreadNum = 0;
+    if(argc==2) {
+        bMulThread = true;
+        nThreadNum = atoi(argv[1]);
+    }
     //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REQ);
@@ -18,9 +24,20 @@ int main ()
 
     //  Do 10 requests, waiting each time for a response
     for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq::message_t request (6);
-        memcpy ((void *) request.data (), "Hello", 5);
-        std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
+        zmq::message_t request (8); //(6);
+        if(bMulThread)
+        {
+            std::string strThreadNum = argv[1];
+            std::string str = strThreadNum + ":Hello";
+            std::cout << "str:" << str <<std::endl;
+            memcpy ((void *) request.data (), str.c_str(), 7); //"Hello", 5);
+            std::string tmp = (char *)request.data ();
+            std::cout << "reguest:" << tmp << std::endl;
+            std::cout << "Thread " << strThreadNum << " Sending Hello " << request_nbr << "..." << std::endl;
+        }else{
+            memcpy ((void *) request.data (), "Hello", 5);
+            std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
+        }
         socket.send (request);
 
         //  Get the reply.
